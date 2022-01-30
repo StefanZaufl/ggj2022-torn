a649d0ef9f2adc7b4b6f3b4b80e2da2e4c879f85
Stefan Zaufl <stefan.zaufl@hotmail.com>
2022-01-30 16:56:13 +0100

    Enemy Done

---
+++ Assets/Code/Character/NPC/VelocityAnimationControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class VelocityAnimationControl : MonoBehaviour
+{
+    Animator animator;
+    Rigidbody thisRigidbody;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        thisRigidbody = GetComponentInParent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 velocity = thisRigidbody.velocity;
+
+        if(velocity.sqrMagnitude > 0.01f)
+        {
+            velocity = velocity.normalized;
+        }
+        else
+        {
+            velocity = Vector2.zero;
+        }
+
+        animator.SetFloat("X", velocity.x);
+        animator.SetFloat("Y", velocity.y);
+    }
+}
