15c877532d98fe7b14be88e016bf96f962fb1a6f
Dieter Dornmayr <developer@gedankenkraft.com>
2022-02-04 20:15:09 +0100

    Implemented Sister with Win-Screen, had to rework Mother ToggleMushroomField Script

--- Assets/Code/Character/NPC/Relative.cs
+++ Assets/Code/Character/NPC/Relative.cs
@@ -2,13 +2,8 @@
 
 public class Relative : MonoBehaviour
 {
-	public GameObject mf;
     private void OnTriggerEnter(Collider other)
-    {
-		
-		mf = GameObject.Find("MushroomField");
-		mf.SetActive(false);
-		
-        //FindObjectOfType<SceneChanger>().ChangeToWin();
+    {		
+        FindObjectOfType<SceneChanger>().ChangeToWin();
     }
 }
