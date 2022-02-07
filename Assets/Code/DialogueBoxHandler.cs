26298ba3974781a93dab88651ea8c95b57593510
d1373r <d1373rgamedev@gedankenkraft.com>
2022-02-07 22:11:52 +0100

    Introducing Component DialogueBoxHandler, kann durch z.B. Box Collider getriggert werden

---
+++ Assets/Code/DialogueBoxHandler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class DialogueBoxHandler : MonoBehaviour
+{
+	public string DialogueBoxToHandle;
+	public string TextMeshProObjectToShow;
+	
+    private void OnTriggerEnter(Collider other)
+    {
+		GameObject db = GameObject.Find(DialogueBoxToHandle);
+		db.GetComponent<CanvasGroup>().alpha = 1;
+		
+		GameObject text = GameObject.Find(TextMeshProObjectToShow);
+		text.GetComponent<CanvasGroup>().alpha = 1;
+		
+        //FindObjectOfType<SceneChanger>().ChangeToWin();
+    }
+	
+	void OnTriggerExit(Collider other) 
+	{
+		//Debug.Log ("Triggered Exit");
+		GameObject db = GameObject.Find(DialogueBoxToHandle);
+		db.GetComponent<CanvasGroup>().alpha = 0;
+		
+		GameObject text = GameObject.Find(TextMeshProObjectToShow);
+		text.GetComponent<CanvasGroup>().alpha = 0;
+	}
+}
