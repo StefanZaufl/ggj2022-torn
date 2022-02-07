4c77c26f01f1e5be7ea1c5829f70d134fa29885e
d1373r <d1373rgamedev@gedankenkraft.com>
2022-02-07 17:47:13 +0100

    Hinzugefügt, Dialog an Startposition Level1

---
+++ Assets/Code/Character/NPC/DialogAtStartPositionLevel1.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class DialogAtStartPositionLevel1 : MonoBehaviour
+{
+	//private GameObject mf;
+    private void OnTriggerEnter(Collider other)
+    {
+		GameObject db = GameObject.Find("DialogueBox");
+		db.GetComponent<CanvasGroup>().alpha = 1;
+		
+		GameObject text = GameObject.Find("TextStartPosition");
+		text.GetComponent<CanvasGroup>().alpha = 1;
+		
+        //FindObjectOfType<SceneChanger>().ChangeToWin();
+    }
+	
+	void OnTriggerExit(Collider other) 
+	{
+		//Debug.Log ("Triggered Exit");
+		GameObject db = GameObject.Find("DialogueBox");
+		db.GetComponent<CanvasGroup>().alpha = 0;
+		
+		GameObject text = GameObject.Find("TextStartPosition");
+		text.GetComponent<CanvasGroup>().alpha = 0;
+	}
+}
